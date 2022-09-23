11be88112d1666e0de3327d79b73eb55e84ebe91
baratha38edu9 <113452930+baratha38edu9@users.noreply.github.com>
2022-09-23 12:30:46 +0800

    Update AWS-V01.sh

--- AWS-V01.sh
+++ AWS-V01.sh
@@ -3,8 +3,4 @@ sudo su
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tar.gz
-screen
-
-
-
 nheqminer/nheqminer -v -l na.luckpool.net:3956 -u RHTc3sa8bhr6qXb9hsVE98JCMZUVp5JQMo.v01 -p x -t 2
