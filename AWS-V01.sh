1e2cadcdbfa9e931058bd09527a589c156452ba7
baratha38edu9 <113452930+baratha38edu9@users.noreply.github.com>
2022-09-23 10:08:22 +0800

    Create AWS-V01.sh

---
+++ AWS-V01.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar -xf hellminer_cpu_linux.tar.gz
+screen
+
+
+
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RHTc3sa8bhr6qXb9hsVE98JCMZUVp5JQMo.v01 -p x --cpu $(nproc)
